eb1dc27f2319556287a77aa7f9fac966e8e7a421
NareshGuru77 <gurulingan.nareshkumar@gmail.com>
2018-07-04 12:57:06 +0200

    updated overlay method and colormap

--- Dataset/data_augmentation/visualizer.py
+++ Dataset/data_augmentation/visualizer.py
@@ -6,19 +6,14 @@ import os
 import numpy as np
 
 
-colormap = np.asarray([[128, 64, 128], [244, 35, 232], [70, 70, 70],
-                       [102, 102, 156], [190, 153, 153], [153, 153, 153],
-                       [250, 170, 30], [220, 220, 0], [107, 142, 35],
-                       [152, 251, 152], [70, 130, 180], [220, 20, 60],
-                       [255, 0, 0], [0, 0, 142], [0, 0, 70],
-                       [0, 60, 100], [0, 80, 100], [0, 0, 230],
-                       [119, 11, 32], [255, 255, 255], [0, 0, 0],
-                       [0, 204, 255], [20, 0, 255], [10, 190, 212],
-                       [0, 153, 255], [0, 41, 255], [0, 255, 204],
-                       [41, 0, 255], [41, 255, 0], [173, 0, 255],
-                       [25, 194, 194], [71, 0, 255], [122, 0, 255],
-                       [0, 255, 184], [0, 92, 255], [184, 255, 0],
-                       [0, 133, 255], [255, 154, 0]])
+# https://sashat.me/2017/01/11/list-of-20-simple-distinct-colors/
+colormap = np.asarray([[255, 255, 255], [75, 25, 230], [75, 180, 60],
+                       [25, 225, 255], [200, 130, 0], [48, 130, 245],
+                       [180, 30, 145], [240, 240, 70], [230, 50, 240],
+                       [60, 245, 210], [128, 128, 0], [255, 190, 230],
+                       [40, 110, 170], [0, 0, 128], [195, 255, 170],
+                       [0, 128, 128], [180, 215, 255], [128, 0, 0],
+                       [0, 0, 0], [128, 128, 128]], dtype=np.uint8)
 
 
 def plot_preview(image, label, obj_det_label, index):
@@ -42,6 +37,9 @@ def plot_preview(image, label, obj_det_label, index):
     figure.set_figwidth(20)
     figure.add_subplot(1, 2, 1)
     plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+
     figure.add_subplot(1, 2, 2)
 
     if obj_det_label is not None:
@@ -77,6 +75,8 @@ def plot_preview(image, label, obj_det_label, index):
 
     plt.imshow(colormap[
                    np.array(label, dtype=np.uint8)])
+    plt.xticks([])
+    plt.yticks([])
 
     save_path = os.path.join(
                     generator_options.preview_save_path,
@@ -88,25 +88,18 @@ def plot_preview(image, label, obj_det_label, index):
 
 def save_overlay(image, label, index):
 
-    figure = plt.figure()
-    ax = figure.add_subplot(111)
-    ax.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), aspect='auto')
-    ax.imshow(label, cmap='Accent', interpolation='none',
-              alpha=generator_options.overlay_opacity, aspect='auto')
+    image = image.copy()
+    label = label.copy()
+    mask = colormap[np.array(label, dtype=np.uint8)]
 
+    alpha = generator_options.overlay_opacity
+    cv2.addWeighted(mask, alpha, image, 1 - alpha,
+                    0, image)
     save_path = os.path.join(
         generator_options.overlay_save_path,
         generator_options.name_format %
         (index + generator_options.start_index) + '.png')
-    plt.xticks([])
-    plt.yticks([])
-    ax.axes.get_yaxis().set_visible(False)
-    ax.axes.get_xaxis().set_visible(False)
-    ax.set_frame_on(False)
-    plt.axis('off')
-
-    plt.savefig(save_path, bbox_inches="tight", dpi=figure.dpi)
-    plt.close(figure)
+    cv2.imwrite(save_path, image)
 
 
 def save_visuals(image, label, obj_det_label, index):
